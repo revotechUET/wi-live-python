e14496775aad3c0fb35c92bebcae9f5bf7a5dc3e
Tung HOANG <tung.hoang@gmail.com>
2019-05-26 22:10:45 +0700

    update

--- test/index.js
+++ test/index.js
@@ -1,8 +1,8 @@
 let express = require('express');
-let pyRunner = require('run-python');
+let pyRunner = require('py-runner');
 const cors = require('cors');
 
-let WSServer = require('live-server');
+let WSServer = require('live-python-server');
 let port = 3030;
 
 let app = express();
@@ -17,7 +17,9 @@ wsServer.post('/run', function(req, res) {
     let key = req.body.key || req.query.key;
     pyRunner(pyFile, log2WS, log2WS, function(code) {
         log2WS("DONE");
-    }, {cwd: '/Users/remurd/workspace/revoltech/run-python/test'});
+    }, {
+        cwd: '/Users/remurd/workspace/revoltech/wi-live-python/py-runner/test'
+    });
     res.send({
         code: 200,
         reason: "Call python started"
@@ -27,29 +29,6 @@ wsServer.post('/run', function(req, res) {
             key: key,
             content: data.toString(),
             error: error
-        })
+        });
     }
 });
-/*
-wsServer.get('/start', function(req, res) {
-    let wiId = req.query.wiId;
-    let key = req.query.key;
-    let i = 0;
-    function kickOff() {
-        if (i < 5) {
-            setTimeout(() => {
-                wsServer.send(wiId, JSON.stringify({
-                    key: key,
-                    content: "This is a message " + i++
-                }));
-                kickOff();
-            }, 500);
-        }
-    }
-    kickOff();
-    res.send({
-        code: 200,
-        reason: 'Success'
-    });
-});
-*/
